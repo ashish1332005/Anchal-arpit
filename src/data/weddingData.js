c96fffb2befe2026672cea32efc288f9f98d34c4
Ashish Sharma <ashishsharma01710171@gmail.com>
2026-09-21 19:12:07 +0530

    Update invocation entry in weddingData.js

--- src/data/weddingData.js
+++ src/data/weddingData.js
@@ -24,7 +24,7 @@ export const WEDDING_DETAILS = {
   invocations: [
     { hi: "|| श्री गणेशाय नमः ||", en: "Shree Ganeshay Namah" },
     { hi: "|| श्री रामचंद्राय नमः ||", en: "Shree Ramchandra Namah" },
-    { hi: "|| श्री सेठ माता री ||", en: "Shree Seth Mata Ri" },
+    { hi: "|| समस्त पितृ देव||", en: "Shree Seth Mata Ri" },
   ],
 
   december11Events: [
